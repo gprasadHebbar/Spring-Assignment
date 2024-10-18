6e7b6063c5cf2f3327c7e79217225122ed7f8931
SKR60 <skreddygabu@gmail.com>
2024-10-18 11:40:56 +0530

    Part-2 : Hibernate/JPA CRUD

---
+++ springassessment2/src/test/java/com/kaarspringassessment/springassessment2/Springassessment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaarspringassessment.springassessment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springassessment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
